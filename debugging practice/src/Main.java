d9b7dec84b551f6273e8295504673e0d8fd9dc38
Vaibhav Mishra <vaibhav__mishra>
2023-02-11 11:29:55 +0530

    Added debugger project

---
+++ debugging practice/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int stops;
+        int fare;
+        stops = 0;
+        fare = 0;
+        stops = stops + 1;
+        fare = fare + 5;
+        stops = stops + 1;
+        fare = fare + 3;
+        stops = stops + 1;
+        fare = fare + 7;
+
+        System.out.println(stops);
+        System.out.println(fare);
+
+    }
+}
